58731a027c4241151b7a85e1ede0079e8e4de8c4
Grandalfwise <minecraft.fun.proservers@gmail.com>
2024-05-27 18:10:41 +1000

    Fixed on click. I think...

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function copyIP() {
   // Copy the text inside the text field
-  navigator.clipboard.writeText("Coming Soon");
+  navigator.clipboard.writeText("Coming Soon")
+    .then(() => {
+      alert('Copied the IP of the Obsidius Network server to your clipboard.');
+    })
+}
 
-  // Alert the copied text
-  alert("Copied the text: " + copyText.value);
+function scrollToSection(sectionId) {
+  let targetSection = document.getElementById(sectionId);
+
+  if (targetSection) {
+      window.scrollTo({
+          top: (targetSection.offsetTop - (window.innerHeight - targetSection.offsetHeight) / 2),
+          behavior: 'smooth'
+      });
+  }
 }
